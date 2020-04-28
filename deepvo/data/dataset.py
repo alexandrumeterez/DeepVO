28b7fa87c1927dc3998ba8ab7c5a1337b9a62d80
Alexandru Meterez <alexandrumeterez@gmail.com>
2020-04-28 17:42:47 +0300

    Added visualization script and finished dataset script

--- deepvo/data/dataset.py
+++ deepvo/data/dataset.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from torch.utils.data import Dataset
 from deepvo.conf.params import *
-from deepvo.utils.utils import load_image, Log
+from deepvo.utils.utils import load_image, Log, get6DoFPose, visualize
 
 TAG = 'KITTIVisualOdometryDataset'
 
@@ -14,11 +14,11 @@ class KITTIVisualOdometryDataset(Dataset):
         self.sequences = train_sequences
         self.size = 0  # Dataset size, with all the poses/images
         self.sequences_sizes = []  # Array with size for each sequence
-        self.poses = self.__load_poses()
+        self.poses = self.load_poses()
         self.transform = transform
         self.trajectory_length = trajectory_length
 
-    def __load_poses(self):
+    def load_poses(self):
         poses = []
         for sequence in self.sequences:
             with open(os.path.join(self.poses_dir, sequence + '.txt')) as f:
@@ -28,16 +28,47 @@ class KITTIVisualOdometryDataset(Dataset):
             self.sequences_sizes.append(len(sequence_poses))
         return poses
 
-    def __load_image(self, sequence, index):
+    def load_image(self, sequence, index):
         # Log(TAG, f'Loading {sequence} : {index}')
         path = os.path.join(self.images_dir, 'sequences', sequence, 'image_2', '%06d' % index + '.png')
-        image = load_image(path)
+        image = np.array(load_image(path))
+        image = np.moveaxis(image, 2, 0)
         return image
+
+    def get_sequence(self, index):
+        total = 0
+        for sequence, seq_size in enumerate(self.sequences_sizes):
+            # If we can fit [index, index + traj_len] in this sequence,
+            # then choose this sequence
+            if index + self.trajectory_length < total:
+                return sequence, total - index
+            total += seq_size
+
+    def __getitem__(self, index):
+        # Find sequence that contains index and the index corespondent in the sequence
+        sequence, index = self.get_sequence(index)
+
+        # Form a batch
+        imgs_batch, odom_batch = [], []
+        for i in range(index, index + self.trajectory_length):
+            img1 = self.load_image(self.sequences[sequence], i)
+            img2 = self.load_image(self.sequences[sequence], i + 1)
+            odom1 = get6DoFPose(self.poses[sequence][i])
+            odom2 = get6DoFPose(self.poses[sequence][i + 1])
+            odom = odom2 - odom1
+            if self.transform:
+                img1 = self.transform(img1)
+                img2 = self.transform(img2)
+            imgs_batch.append(np.concatenate([img1, img2], axis=0))
+            odom_batch.append(odom)
+        return np.array(imgs_batch), np.array(odom_batch)
 
 
 # Test
 if __name__ == '__main__':
     vods = KITTIVisualOdometryDataset(IMAGES_DIR, POSES_DIR, TRAIN_SEQUENCES)
-    print(vods.size)
-    print(vods.sequences_sizes)
-    vods.load_image('00', 10)
+    imgs, odom = vods[5000]
+    visualize(imgs, vods.trajectory_length)
+
+    print(imgs.shape)
+    print(odom.shape)
--- deepvo/utils/utils.py
+++ deepvo/utils/utils.py
@@ -1,4 +1,8 @@
 from PIL import Image
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 def load_image(path):
@@ -7,3 +11,45 @@ def load_image(path):
 
 def Log(tag, message):
     print(f'[{tag}] {message}')
+
+
+def isRotationMatrix(R):
+    I = np.identity(3, dtype=R.dtype)
+    n = np.linalg.norm(I - np.dot(R.T, R))
+    return n < 1e-6
+
+
+def rotationMatrixToEulerAngles(R):
+    assert isRotationMatrix(R)
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z], dtype=np.float32)
+
+
+def get6DoFPose(p):
+    pos = np.array([p[3], p[7], p[11]])
+    R = np.array([[p[0], p[1], p[2]], [p[4], p[5], p[6]], [p[8], p[9], p[10]]])
+    angles = rotationMatrixToEulerAngles(R)
+    return np.concatenate((pos, angles))
+
+
+def visualize(img_arr, length):
+    fig = plt.figure()
+    ims = []
+    for i in range(length):
+        image = img_arr[i, :3, :, :]
+        image = np.moveaxis(image, 0, 2)
+        im = plt.imshow(image, animated=True)
+        ims.append([im])
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+    plt.show()
